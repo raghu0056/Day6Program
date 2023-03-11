badd1f6d016f7a7269799afda3ea62ef724b3ec3
raghu0056 <raghumani0056@gmail.com>
2023-03-11 14:00:58 +0530

    Fibonacci series example program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+ class main{
+    public static void main(String args[])
+    {
+        int n1=0;
+        int n2=1;
+        int n3;
+        int count=10;
+        System.out.print("Fibonacci series example program" +"\n");
+        System.out.print(n1+" "+n2);  //printing first two number int series 0
+        for(int i=2;i<count;i++)    //loop starts from 2 because 0 and 1 are already printed
+        {
+            n3=n1+n2;
+            System.out.print(" "+n3);
+            n1=n2;
+            n2=n3;
+
+        }
+
+    }}
